c22e78de634f37f450bd173bac8f3639c6dc586d
koech-code <nixon.koech@student.moringaschool.com>
2021-05-01 21:05:40 +0300

    added a new line of code

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ var totalProduction=function(shed, litres){
     console.log("Your production in " + shed + litres + " litres per day");
 }
 totalProduction("shed A ","510");
+totalProduction("shed B ","308");
