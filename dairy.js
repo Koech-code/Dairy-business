9b93906ea3160b7587a5c7668465beb7079cc100
koech-code <nixon.koech@student.moringaschool.com>
2021-05-02 19:18:30 +0300

    more lines of codes to javascript file

--- dairy.js
+++ dairy.js
@@ -1,22 +1,29 @@
-let example = document.getElementById("example").value
+let example = document.getElementById("shedA").value
 var totalProduction=function(shed, litres){
 //  console.log("Your production in " + shed + litres + " litres per day");
 return "Your production in " + shed + litres + " litres per day";
 }
-document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",example);
+document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",shedA)
 document.getElementById("total").innerHTML=totalProduction("shed B ","308");
 document.getElementById("all").innerHTML=totalProduction("shed C ","486");
 document.getElementById("last").innerHTML=totalProduction("shed D ","572");
-
-let array=[example,308,486,572];
+// sum earnings per day
+let array=[shedA,308,486,572];
 let sum=array.reduce(function(a,b){
 return a + b;
 }, 0);
 console.log("The total production is "+sum+"litres per day")
 document.getElementById("sub").addEventListener("click", function(){
-    let example = document.getElementById("example").value
-console.log(example)
+    let shedA= document.getElementById("shedA").value
+console.log(shedA)
 
-document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",example);
+document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",shedA);
 console.log("The total production is "+sum+ "litres per day")
 })
+// earnings per week
+let incomeOverTime=(income,time)=>
+console.log("your "+time+"income will be "+income);
+incomeOverTime("590940 ","weekly ")
+
+// earnings per year
+let yearlyIncome=[]
