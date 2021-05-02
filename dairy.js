4181d471927e153ffe44b8c2dba95311bd04b58a
koech-code <nixon.koech@student.moringaschool.com>
2021-05-02 12:40:35 +0300

    corrected my readme name from KOech to Koech

--- dairy.js
+++ dairy.js
@@ -5,3 +5,9 @@ totalProduction("shed A ","510");
 totalProduction("shed B ","308");
 totalProduction("shed C ","486");
 totalProduction("shed D ","572");
+
+let array=[510,308,486,572];
+let sum=array.reduce(function(a,b){
+return a + b;
+}, 0);
+console.log("The total production is ",sum,"litres per day")
