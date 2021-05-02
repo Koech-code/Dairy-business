d97aec886d26c544332874a2c2a9fbddf11f4937
koech-code <nixon.koech@student.moringaschool.com>
2021-05-02 19:58:20 +0300

    added new lines of code  to javascript file

--- dairy.js
+++ dairy.js
@@ -3,7 +3,7 @@ var totalProduction=function(shed, litres){
 //  console.log("Your production in " + shed + litres + " litres per day");
 return "Your production in " + shed + litres + " litres per day";
 }
-document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",shedA)
+document.getElementById("totalProduction").innerHTML=totalProduction("shed A ",shedA);
 document.getElementById("total").innerHTML=totalProduction("shed B ","308");
 document.getElementById("all").innerHTML=totalProduction("shed C ","486");
 document.getElementById("last").innerHTML=totalProduction("shed D ","572");
@@ -26,4 +26,11 @@ console.log("your "+time+"income will be "+income);
 incomeOverTime("590940 ","weekly ")
 
 // earnings per year
-let yearlyIncome=[]
+let yearlyIncome=[2617020,2448180,2617020,2532600,2617020,2532600,2617020,2617020,2532600,2617020,2532600,2617020]
+let totals=yearly.reduce(function(jan,dec){
+    return jan+dec;
+}, 0);
+console.log(totals);
+var january={
+
+}
